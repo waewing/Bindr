991a8d37f546cc0df43cc5fb0693fb59a5d32daa
waewing <waewing24@gmail.com>
2025-06-02 14:17:01 -0400

    Tweaking Render

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 
 //MiddleWare
-app.use(cors());
+app.use(cors({
+  origin: "https://bindr-evbw.onrender.com/",
+  credentials: true // if using cookies/auth
+}));
 app.use(express.json());
 
 //Serve React Frontend
