48436d581f78f728380353b253046455ca567c20
waewing <waewing24@gmail.com>
2025-06-02 13:44:28 -0400

    Tweaking Render

--- server/server.js
+++ server/server.js
@@ -14,11 +14,20 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//Serve React Frontend
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 //Routes
 app.use("/", OnePiece);
 app.use("/", UserProfile);
 
+<<<<<<< Updated upstream
+=======
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+})
+
+>>>>>>> Stashed changes
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
